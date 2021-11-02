dcc6a0cdb4b604a00c45b373ea8039b4bcf474be
Eliezer Moraes <eliezer.moraes@outlook.com>
2021-11-02 18:45:57 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/eliezer/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.eliezer.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

93d07d51fe852015ae3e5bfd88e32e0d3b26f3ec
Eliezer Moraes <eliezer.moraes@outlook.com>
2021-11-02 20:46:52 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/eliezer-moraes-silva-80b68010b/" target="_blank" rel="noreferrer">Eliezer Moraes Silva</a></p>
+          <p className="text-light">Meu <a href="https://github.com/eliezermoraesss" target="_blank" rel="noreferrer">GitHub</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  

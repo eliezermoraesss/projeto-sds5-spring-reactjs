3f28a3b6aaf98327f93cb227befa0f676dbdb0fc
Eliezer Moraes <eliezer.moraes@outlook.com>
2021-11-04 22:10:15 -0300

    Grouped by search

--- backend/src/main/java/com/devsuperior/eliezer/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/eliezer/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.devsuperior.eliezer.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.eliezer.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.eliezer.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.eliezer.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 	
+	@Query("SELECT new com.devsuperior.eliezer.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.devsuperior.eliezer.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }

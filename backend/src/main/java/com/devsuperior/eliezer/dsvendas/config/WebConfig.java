0d0caaa0d6cb54d88b34f33443fbc4eaf0833c86
Eliezer Moraes Silva <77923494+eliezermoraesss@users.noreply.github.com>
2023-06-18 15:33:15 -0300

    Update WebConfig.java

--- backend/src/main/java/com/devsuperior/eliezer/dsvendas/config/WebConfig.java
+++ backend/src/main/java/com/devsuperior/eliezer/dsvendas/config/WebConfig.java
@@ -9,17 +9,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
-	@Value("${cors.origins}")
-	private String corsOrigins;
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
-			}
-		};
-	}
-
 }
